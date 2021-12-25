750243ff451b56a973cadd92e17378187f364939
Wellerson <wllrsn.c@gmail.com>
2021-12-25 14:21:22 -0300

    feat: added config domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  domains: [
+    {
+      domain: "wellerson.dev",
+      defaultLocale: "pt-BR",
+    },
+    {
+      domain: "www.wellerson.dev",
+      defaultLocale: "pt-BR",
+    },
+  ],
+};
