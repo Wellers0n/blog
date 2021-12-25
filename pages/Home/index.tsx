62c1b41b5b8b2ef8e4d54c271a7329d379e8d45c
Wellerson <wllrsn.c@gmail.com>
2021-12-25 20:15:50 -0300

    refactor: removed Home folder

--- pages/Home/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from "next";
-import { Container } from "./styles";
-
-const Home: NextPage = () => {
-  return <Container>home page</Container>;
-};
-
-export default Home;
