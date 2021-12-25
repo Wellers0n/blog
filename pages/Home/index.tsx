ad55a393a6f4c08fe0682dc99a9851c119e67940
Wellerson <wllrsn.c@gmail.com>
2021-12-25 13:28:01 -0300

    test

--- pages/Home/index.tsx
+++ pages/Home/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import { Container } from "./styles";
 
 const Home: NextPage = () => {
-  return <Container>home page</Container>;
+  return <Container>homee page</Container>;
 };
 
 export default Home;
