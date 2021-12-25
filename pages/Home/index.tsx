d0e88e02c0a1588601c1786c1e19d8cfef922f20
Wellerson <wllrsn.c@gmail.com>
2021-12-25 12:51:24 -0300

    first commit

---
+++ pages/Home/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import { Container } from "./styles";
+
+const Home: NextPage = () => {
+  return <Container>home page</Container>;
+};
+
+export default Home;
